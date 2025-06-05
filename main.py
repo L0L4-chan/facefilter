cda41e7a292e4166414c66a314831b891551ca1c
L0L4-chan <gaiafwfg@gmail.com>
2025-06-05 20:27:28 +0200

    limpieza de codigo

--- main.py
+++ main.py
@@ -2,15 +2,7 @@
 #from utils import procesar_imagen
 
 from utils import demo
-'''
-interface = gr.Interface(fn = procesar_imagen, 
-                         input = gr.Image(source = "upload",
-                                          tool = "editor",
-                                          label ="Upload your image or use your camera"),
-                         outputs = gr.Image(label = "result"),
-                         title = "AI Glasses filter",
-                         description = "Using VA technology, try some glasses.")
-'''
+
 if __name__ == "__main__":
     #interface.launch()
     demo().launch()
